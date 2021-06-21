dd33ce73b378678c91e90f8ded47902b5ff5ea1e
Jrferrao <jrferrao@gmail.com>
2021-06-20 21:22:34 -0300

    ProjetoJava

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -124,79 +124,189 @@ function comparaDoisNumeros(num1, num2) {
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
 
+  let arrayResposta = []
+  let menorNumero = Infinity
+  let maiorNumero = 0
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maiorNumero) {
+      maiorNumero = array[i]
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < menorNumero) {
+      menorNumero = array[i]
+    }
+  }
+  
+  arrayResposta.push(maiorNumero)
+  arrayResposta.push(menorNumero)
+return arrayResposta
 }
 
 // EXERCÍCIO 11
 function ordenaArray(array) {
-
+  const ordenado = array.sort(function(a, b){
+    return a - b
+  })
+  return ordenado
 }
 
 // EXERCÍCIO 12
 function filmeFavorito() {
-
+  const filmeAstrodev = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"],
+  }
+return filmeAstrodev
 }
 
 // EXERCÍCIO 13
 function imprimeChamada() {
-  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  const filmeAstrodev = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"],
+  }
+ 
+  return `Venha assistir ao filme ${filmeAstrodev.nome}, de ${filmeAstrodev.ano}, dirigido por ${filmeAstrodev.diretor} e estrelado por ${filmeAstrodev.atores[0]}, ${filmeAstrodev.atores[1]}, ${filmeAstrodev.atores[2]}, ${filmeAstrodev.atores[3]}.`
+
 }
 
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
+  const objetoRetangulo = {
+    largura: lado1,
+    altura: lado2,
+    perimetro: 2 * (lado1 + lado2),
+    area: lado1 * lado2
+  }
+  return objetoRetangulo
 
 }
 
 // EXERCÍCIO 15
 function anonimizaPessoa(pessoa) {
-
+  const pessoa1 = {
+    nome: "João",
+    idade: 34,
+    email: "jrferrao@gmail.com",
+    endereco: "Rua do Futuro, 4"
+  }
+  const anonimo = {
+    ...pessoa,
+    nome: "ANÔNIMO",
+  }
+  return anonimo
 }
 
 // EXERCÍCIO 16A
 function maioresDe18(arrayDePessoas) {
+  
+  const usuarios = [
+    { nome: "Pedro", idade: 20 },
+    { nome: "João", idade: 10 },
+    { nome: "Paula", idade: 12 },
+    { nome: "Artur", idade: 89 } 
+  ]
 
+  let maiorDeIdade = arrayDePessoas.filter((arrayDePessoas) => {
+    return arrayDePessoas.idade >= 18
+  })
+
+  return maiorDeIdade
 }
 
 // EXERCÍCIO 16B
 function menoresDe18(arrayDePessoas) {
+  let menorDeIdade = arrayDePessoas.filter((arrayDePessoas) => {
+    return arrayDePessoas.idade < 18
+  })
 
+  return menorDeIdade
 }
 
 // EXERCÍCIO 17A
 function multiplicaArrayPor2(array) {
-
+  const multiplicaPorDois = array.map((numero) => {
+    return numero * 2
+  })
+  return multiplicaPorDois
 }
 
 // EXERCÍCIO 17B
 function multiplicaArrayPor2S(array) {
-
+  const multiplicaPorDois = multiplicaArrayPor2(array)
+  const multiplicaPorDoisString = multiplicaPorDois.map((numero) => {
+    return numero.toString()
+  })
+  return multiplicaPorDoisString
+  
 }
 
 // EXERCÍCIO 17C
 function verificaParidade(array) {
-
+  const paridade = array.map((numero) => {
+    if(numero % 2 === 0) {
+      return `${numero} é par`
+    } else {
+      return `${numero} é ímpar`
+    }
+  })
+  return paridade
 }
 
 // EXERCÍCIO 18A
 function retornaPessoasAutorizadas(pessoas) {
-
+  const autorizadas = pessoas.filter((condicao) => {
+    if ((condicao.altura >= 1.5) && (condicao.idade > 14) && (condicao.idade < 60)) {
+      return condicao
+    }
+  })
+  return autorizadas
 }
 
 // EXERCÍCIO 18B
 function retornaPessoasNaoAutorizadas(pessoas) {
-
+  const naoAutorizadas = pessoas.filter((condicao) => {
+    if (!((condicao.altura >= 1.5) && (condicao.idade > 14) && (condicao.idade < 60))) {
+      return condicao
+    }
+  })
+  return naoAutorizadas
 }
 
 // EXERCÍCIO 19A
 function ordenaPorNome(consultasNome) {
+ 
+  const nomesEmOrdem = consultasNome.sort(function(a,b) {
+    return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0)
+  })
+  return nomesEmOrdem
 
 }
 
 // EXERCÍCIO 19B
 function ordenaPorData(consultasData) {
+  function consultarData(a,b) {
+    return a.dataDaConsulta < b.dataDaConsulta 
+  }
 
+  return consultasData.sort(consultarData)
 }
 
 // EXERCÍCIO 20
 function calculaSaldo(contas) {
-
+  for (let i=0; i<contas.length; i++){
+    let somaGastos = 0    
+    for (gastos of contas[i].compras){
+      somaGastos += gastos
+    }
+    contas[i].saldoTotal = contas[i].saldoTotal - somaGastos
+    console.log(contas[i].saldoTotal)
+  }
+  return contas
 }
